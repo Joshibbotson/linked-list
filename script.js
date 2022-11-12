86233221488b741b8f7d8f4673a50b9ee1b7cde4
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-11-12 09:40:19 +0000

    updated linkedlist for returnHead, tail and index functions

--- script.js
+++ script.js
@@ -1,28 +1,85 @@
 class LinkedList {
     constructor() {
-        this.console = () => {
-            console.log()
+        this.head = null
+        this.size = 0
+    }
+
+    prepend = value => {
+        this.head = new Node(value, this.head)
+        this.size++
+    }
+
+    append = value => {
+        let node = new Node(value)
+        let current
+
+        if (!this.head) {
+            this.head = node
+            this.size++
+        } else {
+            current = this.head
+            while (current.nextNode) {
+                current = current.nextNode
+            }
+            current.nextNode = node
+            this.size++
         }
     }
+
+    printAll = () => {
+        let current = this.head
+        while (current) {
+            console.log(current.value)
+            current = current.nextNode
+        }
+    }
+
+    size = () => {
+        console.log(this.size)
+    }
+
+    returnHead = () => {
+        if (!this.head) {
+            console.log("No head defined")
+        } else {
+            return this.head
+        }
+    }
+
+    tail = () => {
+        if (this.size === 0) {
+            console.log("size is 0")
+        } else {
+            let current = this.head
+            for (let i = 0; i <= this.size - 2; i++) {
+                current = current.nextNode
+                if (i === this.size - 2) {
+                    return current
+                }
+            }
+        }
+    }
+    index = index => {
+        if (this.size === 0) {
+            console.log("size is 0")
+        } else {
+            let current = this.head
+            let size = this.size
+            size = 1
+            while (size !== index) {
+                current = current.nextNode
+                size++
+            }
+            return current
+        }
+    }
+    pop = () => {}
 }
 
+// Node class//
 class Node {
-    constructor() {
-        this.value = null
-        this.nextNode = null
-        this.setValue = (value, node) => {
-            this.value = value
-            this.nextNode = node
-        }
+    constructor(value, pointer) {
+        this.value = value
+        this.nextNode = pointer
     }
 }
-
-const n1 = new Node()
-const n2 = new Node()
-const n3 = new Node()
-
-n1.setValue(100, n2)
-n2.setValue(200, n3)
-n3.setValue(300, null)
-
-console.log(n1)
